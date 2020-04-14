4794921d463a3cdb94ea862724b475ede666dfee
jkean <joshkn@protonmail.com>
2020-04-14 15:15:53 -0700

    added a search contract feature

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from contract import Contract
 
+list_of_contracts = []
+
+search_contract("ContractName")
+
+def seach_contract(search_paramater):
+
+    for contract in list_of_contracts:
+        if search_paramater in [contract.ContractName, contract.PartyA, contract.PartyB,
+                                contract.ContractText, contract.Ownership, contract.PaymentTerms,
+                                contract.ContractValue, contract.DeliveryDate, self.ExpiryDate]:
+            return contract
+
+
+
 def CreateContract(ContractName, PartyA, PartyB, ContractText, Ownership, PaymentTerms, ContractValue, DeliveryDate, ExpiryDate):
     return Contract(ContractName, PartyA, PartyB, ContractText, Ownership, PaymentTerms, ContractValue, DeliveryDate, ExpiryDate)
