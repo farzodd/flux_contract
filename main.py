cb27786f9e386e60de0091a101b047207621869b
jkean <joshkn@protonmail.com>
2020-04-14 15:47:19 -0700

    create contract updated to append contract to list

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def seach_contract(search_paramater):
 
 
 def CreateContract(ContractName, PartyA, PartyB, ContractText, Ownership, PaymentTerms, ContractValue, DeliveryDate, ExpiryDate):
-    return Contract(ContractName, PartyA, PartyB, ContractText, Ownership, PaymentTerms, ContractValue, DeliveryDate, ExpiryDate)
+    list_of_contracts.append(Contract(ContractName, PartyA, PartyB, ContractText, Ownership, PaymentTerms, ContractValue, DeliveryDate, ExpiryDate))
